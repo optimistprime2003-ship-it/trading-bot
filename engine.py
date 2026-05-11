5819459634655c50901e17febd7fd7a775c73adb
optimistprime2003-ship-it <optimistprime2003@gmail.com>
2026-05-11 11:44:13 +0100

    engine.py

--- engine.py
+++ engine.py
@@ -14,8 +14,8 @@ RISK_PER_TRADE = 0.01
 MAX_ALLOWED_SPREAD_PIPS = 3.0
 
 API_KEYS = [
-    os.environ.get("KEY_ONE", "keyone"),
-    os.environ.get("KEY_TWO", "keytwo")
+    os.environ.get("KEY_ONE", "d93af08b103e43c99034dd6362a239d3"),
+    os.environ.get("KEY_TWO", "738fd3d524944eadba4f533fe8832525")
 ]
 
 CURRENT_KEY_INDEX = 0
